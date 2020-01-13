3670cd37b6be97f63bdfef44d33a3bc9b978e002
Gyula Gubacsi <gyula.gubacsi@gmail.com>
2020-01-13 21:58:22 +0000

    refactor: introducing points for change the polynomial type
    The PSI algorithm is using Additive ElGamal encryption system, which is basically just using the g^m (g = generator, m = message) instead of g. This way, the message will not be completely decipherable, but identity is enough.
    
    In this form, we only interested in the Polynomial to accept a Field type, which in this case will be `int`, but later this will be replaced with the cipher type of the Additive ElGamal crypto-system.

--- test/RoundTrip.Test.cpp
+++ test/RoundTrip.Test.cpp
@@ -5,43 +5,51 @@
 #include <vector>
 
 
-auto operator*(std::vector<int> const& lhs, std::vector<int> const& rhs) -> std::vector<int> {
-  std::vector<int> result;
-  result.resize(lhs.size() + rhs.size() - 1, 0);
-  for (int i = 0; i < lhs.size(); ++i) {
-    for (int j = 0; j < rhs.size(); ++j) {
-      result[i + j] += lhs[i] * rhs[j];
+template<typename Integral> using Polynomial = std::vector<Integral>;
+
+template<typename Integral>
+  auto operator*(Polynomial<Integral> const& lhs, Polynomial<Integral> const& rhs) -> Polynomial<Integral> {
+    Polynomial<Integral> result;
+    result.resize(lhs.size() + rhs.size() - 1, 0);
+    for (int i = 0; i < lhs.size(); ++i) {
+      for (int j = 0; j < rhs.size(); ++j) {
+        result[i + j] += lhs[i] * rhs[j];
+      }
     }
+    return result;
   }
-  return result;
-}
 
 
-auto prepare(std::vector<int> const& numbers) -> std::vector<int> {
+using Field = int;
+
+
+auto prepare(std::vector<int> const& numbers) -> Polynomial<Field> {
   // Turn the numbers into coefficients of a polynomial with the roots of the set
-  auto result = std::vector<int>{1};
+  auto result = Polynomial<int>{1};
   for (int i = 0; i < numbers.size(); i++) {
-    result = result * std::vector{-1 * numbers[i], 1};
+    result = result * Polynomial<int>{-1 * numbers[i], 1};
   }
+
   return result;
 }
 
 
-auto evaluate(std::vector<int> const& polynomial, std::vector<int> const& set) -> std::vector<int> {
-  auto result = std::vector<int>{};
-  for (auto i = 0u; i < set.size(); ++i) {
-    auto s = 0;
-    for (auto j = 0u; j < polynomial.size(); ++j) {
-      auto const x_pow = std::pow(set[i], j);
-      s += polynomial[j] * x_pow;
+template<typename Integral>
+  auto evaluate(Polynomial<Field> const& polynomial, std::vector<Integral> const& set) -> std::vector<Field> {
+    auto result = std::vector<Integral>{};
+    for (auto i = 0u; i < set.size(); ++i) {
+      auto s = 0;
+      for (auto j = 0u; j < polynomial.size(); ++j) {
+        auto const x_pow = std::pow(set[i], j);
+        s += polynomial[j] * x_pow;
+      }
+      result.push_back(s + set[i]);
     }
-    result.push_back(s + set[i]);
+    return result;
   }
-  return result;
-}
 
 
-auto extract_intersection(std::vector<int> const& evaluated_set, std::vector<int> const& original) -> std::vector<int> {
+auto extract_intersection(std::vector<Field> const& evaluated_set, std::vector<int> const& original) -> std::vector<int> {
   auto intersection = std::vector<int>{};
   for (auto i = 0u; i < evaluated_set.size(); ++i) {
     auto const elem_in_local = std::find(
